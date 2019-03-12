88bd0e52992578fbce85158a6498d248bc9a4153
OgheneMichael <zonalmeet@gmail.com>
2019-03-12 15:10:53 +0100

    Code for URL Parameters with React Router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React, { Component } from "react";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-const Home = () => (
+const Child = ({ match }) => (
 	<div>
-		<h2>Home</h2>
-	</div>
-);
-
-const About = () => (
-	<div>
-		<h2>About</h2>
-	</div>
-);
-
-const Topic = ({ match }) => (
-	<div>
-		<h2>{match.params.topicId}</h2>
-	</div>
-);
-
-const Topics = ({ match }) => (
-	<div>
-		<h2>Topics</h2>
-		<ul>
-			<li>
-				<Link to={`${match.url}/rendering`}>Rendering with React</Link>
-			</li>
-			<li>
-				<Link to={`${match.url}/components`}>Components</Link>
-			</li>
-			<li>
-				<Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-			</li>
-		</ul>
-
-		<Route path="/topics/:topicId" component={Topic} />
-		<Route
-			exact
-			path={match.url}
-			render={() => <h3>Please select a topic</h3>}
-		/>
+		<h3>ID: {match.params.id}</h3>
 	</div>
 );
 
@@ -48,23 +12,23 @@ class App extends Component {
 		return (
 			<Router>
 				<div>
+					<h2>Accounts</h2>
 					<ul>
 						<li>
-							<Link to="/">Home</Link>
+							<Link to="/netflix">Netflix</Link>
 						</li>
 						<li>
-							<Link to="/about">About</Link>
+							<Link to="/zillow-group">Zillow Group</Link>
 						</li>
 						<li>
-							<Link to="/topics">Topics</Link>
+							<Link to="/yahoo">Yahoo</Link>
+						</li>
+						<li>
+							<Link to="/modus-create">Modus Create</Link>
 						</li>
 					</ul>
 
-					<hr />
-
-					<Route exact path="/" component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/topics" component={Topics} />
+					<Route path="/:id" component={Child} />
 				</div>
 			</Router>
 		);
