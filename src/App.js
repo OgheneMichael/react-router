0cdc95e193f9c34181e770619af376f2f25858e2
OgheneMichael <zonalmeet@gmail.com>
2019-03-30 05:00:47 +0100

    Code for Custom Link component with React Router

--- src/App.js
+++ src/App.js
@@ -2,69 +2,46 @@ import React, { Component } from "react";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const routes = [
-	{
-		path: "/",
-		exact: true,
-		sidebar: () => <div>home!</div>,
-		main: () => <h2>Home</h2>
-	},
-	{
-		path: "/bubblegum",
-		sidebar: () => <div>bubblegum!</div>,
-		main: () => <h2>BubbleGum</h2>
-	},
-	{
-		path: "/shoelaces",
-		sidebar: () => <div>shoelaces!</div>,
-		main: () => <h2>Shoelaces</h2>
-	}
-];
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+);
+
+const OldSchoolMenuLink = ({ children, to, exact }) => {
+	return (
+		<Route
+			path={to}
+			exact={exact}
+			children={({ match }) => (
+				<div className={match ? "active" : ""}>
+					{match ? ">" : ""}
+					<Link to={to}>{children}</Link>
+				</div>
+			)}
+		/>
+	);
+};
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div style={{ display: "flex" }}>
-					<div
-						style={{
-							padding: "10px",
-							width: "40%",
-							background: "#f0f0f0"
-						}}
-					>
-						<ul style={{ listStyleType: "none", padding: 0 }}>
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/bubblegum">BubbleGum</Link>
-							</li>
-							<li>
-								<Link to="/shoelaces">Shoelaces</Link>
-							</li>
-						</ul>
+				<div>
+					<OldSchoolMenuLink exact={true} to="/">
+						Home
+					</OldSchoolMenuLink>
+					<OldSchoolMenuLink to="/about">About</OldSchoolMenuLink>
+					<hr />
 
-						{routes.map(route => (
-							<Route
-								key={route.path}
-								path={route.path}
-								exact={route.exact}
-								component={route.sidebar}
-							/>
-						))}
-					</div>
-
-					<div style={{ flex: 1, padding: "10px" }}>
-						{routes.map(route => (
-							<Route
-								key={route.path}
-								path={route.path}
-								exact={route.exact}
-								component={route.main}
-							/>
-						))}
-					</div>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
 				</div>
 			</Router>
 		);
