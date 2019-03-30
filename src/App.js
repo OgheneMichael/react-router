03cd6a7cc637a0e780fc5b0dc43e93218c602414
OgheneMichael <zonalmeet@gmail.com>
2019-03-30 04:11:13 +0100

    Code for Pass Props Link with React Router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React, { Component } from "react";
 
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const About = () => <h1>About</h1>;
-const Company = () => <h1>Company</h1>;
-const User = ({ match }) => (
-	<div>
-		<h2>User: {match.params.user}</h2>
-	</div>
-);
+<Link to={{
+	pathname: '/oghenemichael',
+	state: {
+		fromNotification: true
+	}
+}}>Oghene Michael</Link>
 
-class App extends Component {
+class Profile extends Component {
+	componentDidMount() {
+		const {handle} = this.props.match.params;
+		const {fromNotification} = this.props.location.state
+	}
+
 	render() {
 		return (
-			<Router>
-				<div>
-					<ul>
-						<li>
-							<Link to="/about">About</Link>
-						</li>
-						<li>
-							<Link to="/company">Company</Link>
-						</li>
-						<li>
-							<Link to="/kim">Kim</Link>
-						</li>
-						<li>
-							<Link to="/chris">Chris</Link>
-						</li>
-					</ul>
-					<Switch>
-						<Route path="/about" exact component={About} />
-						<Route path="/company" component={Company} />
-						<Route path="/:user" component={User} />
-					</Switch>
-				</div>
-			</Router>
+
 		);
 	}
 }
 
-export default App;
+export default Profile;
