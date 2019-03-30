48a6966fac1daa1ee49444b94c1481f6d6a93deb
OgheneMichael <zonalmeet@gmail.com>
2019-03-30 04:29:51 +0100

    Code for Sidebar or Breadcrumbs with React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-<Link to={{
-	pathname: '/oghenemichael',
-	state: {
-		fromNotification: true
+const routes = [
+	{
+		path: "/",
+		exact: true,
+		sidebar: () => <div>home!</div>,
+		main: () => <h2>Home</h2>
+	},
+	{
+		path: "/bubblegum",
+		sidebar: () => <div>bubblegum!</div>,
+		main: () => <h2>BubbleGum</h2>
+	},
+	{
+		path: "/shoelaces",
+		sidebar: () => <div>shoelaces!</div>,
+		main: () => <h2>Shoelaces</h2>
 	}
-}}>Oghene Michael</Link>
+];
 
-class Profile extends Component {
-	componentDidMount() {
-		const {handle} = this.props.match.params;
-		const {fromNotification} = this.props.location.state
-	}
-
+class App extends Component {
 	render() {
 		return (
+			<Router>
+				<div style={{ display: "flex" }}>
+					<div
+						style={{
+							padding: "10px",
+							width: "40%",
+							background: "#f0f0f0"
+						}}
+					>
+						<ul style={{ listStyleType: "none", padding: 0 }}>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/bubblegum">BubbleGum</Link>
+							</li>
+							<li>
+								<Link to="/shoelaces">Shoelaces</Link>
+							</li>
+						</ul>
 
+						{routes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								exact={route.exact}
+								component={route.sidebar}
+							/>
+						))}
+					</div>
+
+					<div style={{ flex: 1, padding: "10px" }}>
+						{routes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								exact={route.exact}
+								component={route.main}
+							/>
+						))}
+					</div>
+				</div>
+			</Router>
 		);
 	}
 }
 
-export default Profile;
+export default App;
